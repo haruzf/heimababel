e164e7660a569f04a8438d686b42cfd77c11629b
haruzf <10240933+ahru@user.noreply.gitee.com>
2022-07-07 21:35:04 +0800

    i postcss-loader

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,13 @@ module.exports = {
     module: {
         rules: [{
             test: /\.css$/,
-            use: ['style-loader', 'css-loader']
+            use: ['style-loader', 'css-loader', 'postcss-loader']
+        }, {
+            test: /\.less$/,
+            use: ['style-loader', 'css-loader', 'less-loader']
+        }, {
+            test: /\.scss$/,
+            use: ['style-loader', 'css-loader', 'sass-loader']
         }, ]
     }
 
